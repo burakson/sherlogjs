cb3666611a8fbb458b80937345330f180fb1d1d5
Burak Son <hello@burakson.com>
2014-07-29 20:47:20 +0200

    add `watch` task to the gulpfile as a dev dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,32 @@ var config  = require('./config/config.json')
   , sass    = require('gulp-sass')
   , uglify  = require('gulp-uglify')
   , rename  = require('gulp-rename')
-  , replace = require('gulp-replace');
+  , replace = require('gulp-replace')
+  , watch   = require('gulp-watch');
+
+var paths = {
+  js   : 'assets/js/*.js',
+  scss : 'assets/scss/sherlog.scss'
+};
 
 gulp.task('scripts', function() {
-  return gulp.src('assets/js/*.js')
+  return gulp.src(paths.js)
              .pipe(replace('{{sherlog_url}}', config.server_url))
              .pipe(uglify())
-             .pipe(rename({ suffix: '.min'}))
+             .pipe(rename({suffix: '.min'}))
              .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss/sherlog.scss')
-             .pipe(sass({ outputStyle: 'compressed' }))
-             .pipe(rename({ suffix: '.min'}))
+  return gulp.src(paths.scss)
+             .pipe(sass({outputStyle: 'compressed'}))
+             .pipe(rename({suffix: '.min'}))
              .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.js,    ['scripts']);
+    gulp.watch(paths.scss,  ['sass']);
 });
 
 gulp.task('default', ['scripts', 'sass']);
