dcc280fb07b112ee288032af67daff9920d89b4e
Burak Son <hello@burakson.com>
2014-08-04 22:37:08 +0200

    minify dashboard assets.

--- app/common/constants.js
+++ app/common/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-    tracking_types : {
-      'all'   : -1,
-      'error' : 0,
-      'event' : 1,
-      'xhr'   : 2
-    }
-  };
+  tracking_types : {
+    'all'   : -1,
+    'error' : 0,
+    'event' : 1,
+    'xhr'   : 2
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,37 @@ var config  = require('./config/config.json')
   , rename  = require('gulp-rename')
   , replace = require('gulp-replace')
   , watch   = require('gulp-watch')
-  , jshint = require('gulp-jshint');
-
+  , concat  = require('gulp-concat');
 
 var paths = {
-  js : 'assets/js/*.js',
-  scss : 'assets/scss/sherlog.scss',
-  scssToWatch : 'assets/scss/**/*.scss'
+  js : {
+    all       : 'assets/js/*.js',
+    framework : 'assets/js/sherlog.js',
+    dashboard : 'assets/js/dashboard.js'
+  },
+  scss : {
+    all  : 'assets/scss/**/*.scss',
+    main : 'assets/scss/sherlog.scss'
+  }
 };
 
-gulp.task('scripts', function() {
-  return gulp.src(paths.js)
+gulp.task('dashboard-scripts', function() {
+  // include all dependencies along with dashboard script
+  var dependencies = [ 
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/datatables/media/js/jquery.dataTables.js',
+    'bower_components/datatables-bootstrap3/BS3/assets/js/datatables.js',
+    paths.js.dashboard
+  ];
+
+  return gulp.src(dependencies)
+      .pipe(concat('dashboard.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('framework-script', function() {
+  return gulp.src(paths.js.framework)
              .pipe(replace('{{sherlog_url}}', config.server_url))
              .pipe(replace('{{pixel_name}}',  config.pixel_name))
              .pipe(uglify())
@@ -24,21 +44,15 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('sass', function() {
-  return gulp.src(paths.scss)
+  return gulp.src(paths.scss.main)
              .pipe(sass({outputStyle: 'compressed'}))
              .pipe(rename({suffix: '.min'}))
              .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('lint', function() {
-  return gulp.src(paths.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
 gulp.task('watch', function() {
-    gulp.watch(paths.js,    ['scripts']);
-    gulp.watch(paths.scssToWatch,  ['sass']);
+    gulp.watch(paths.js.all, ['dashboard-scripts', 'framework-script']);
+    gulp.watch(paths.scss.all, ['sass']);
 });
 
-gulp.task('default', ['scripts', 'sass']);
+gulp.task('default', ['dashboard-scripts', 'framework-script', 'sass']);
